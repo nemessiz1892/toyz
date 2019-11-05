00ad59e7521f3272de64014bdcf9ab72ef67d637
nemessiz1892 <57384447+nemessiz1892@users.noreply.github.com>
2019-11-04 23:16:00 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+//require_once "/var/www/html/nextServer/ini.php";
+require_once "controladores/plantilla_controlador.php";
+/*require_once "controladores/proveedores_controlador.php";
+require_once "controladores/existencias_controlador.php";
+require_once "controladores/pagos_controlador.php";
+require_once "controladores/OrdCompras_controlador.php";
+require_once "controladores/password_controlador.php";
+require_once "controladores/sellOut_controlador.php";
+
+
+
+
+
+
+require_once "modelos/proveedores_modelos.php";
+require_once "modelos/existencias_modelos.php";
+require_once "modelos/conexion_modelo.php";
+require_once "modelos/pagos_modelos.php";
+require_once "modelos/ordenCompra_modelos.php";
+require_once "modelos/password_modelos.php";
+require_once "modelos/sellOut_modelos.php";
+*/
+
+
+
+
+
+
+
+//require_once "modelos/urls.php";
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> plantilla();
+
+ ?>
+
+
+
+
